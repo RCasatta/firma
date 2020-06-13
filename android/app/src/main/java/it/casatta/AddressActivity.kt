d15b8fed8cc2dd3c453cc0857c5dda8b9737e9f5
Riccardo Casatta <riccardo@casatta.it>
2020-06-13 20:32:19 +0200

    use info button for informations

--- android/app/src/main/java/it/casatta/AddressActivity.kt
+++ android/app/src/main/java/it/casatta/AddressActivity.kt
@@ -3,6 +3,7 @@ package it.casatta
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_address.*
 
 class AddressActivity : AppCompatActivity() {
@@ -19,6 +20,8 @@ class AddressActivity : AppCompatActivity() {
 
         address.text = addressData.address
         path.text = addressData.path
+
+        info_address.setOnClickListener { AlertDialog.Builder(this).setTitle("Attention").setMessage("Double check this address ant the path with the ones generated with the online tool. This prevents an attacker replacing the address generated with the online tool with an address he owns.").create().show() }
     }
 
 }
