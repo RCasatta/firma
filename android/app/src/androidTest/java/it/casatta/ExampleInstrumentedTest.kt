cfb24cb492e38f1fe03d9d87c98e0efe8c009c35
Riccardo Casatta <riccardo@casatta.it>
2020-08-21 23:22:17 +0200

    add UI test step

--- android/app/src/androidTest/java/it/casatta/ExampleInstrumentedTest.kt
+++ android/app/src/androidTest/java/it/casatta/ExampleInstrumentedTest.kt
@@ -1,12 +1,21 @@
 package it.casatta
 
-import androidx.test.platform.app.InstrumentationRegistry
+import android.content.Intent
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.ActivityTestRule
+import org.junit.Assert.assertTrue
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -19,6 +28,27 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("it.casatta", appContext.packageName)
+        assertTrue(appContext.packageName.startsWith("it.casatta"))
+    }
+
+    @get:Rule
+    var activityRule: ActivityTestRule<MainActivity> = ActivityTestRule(
+        MainActivity::class.java,
+        true,  // initialTouchMode
+        false
+    ) // launchActivity. False to customize the intent
+
+
+    @Test
+    fun performClick() {
+        activityRule.launchActivity(Intent())
+
+        onView(withId(R.id.key_button)).perform(click())
+
+        onView(withId(R.id.item_new)).perform(click())
+
+        onView(withId(R.id.items_list)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()));
+        //onView(withId(R.id.items_list)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()));
+
     }
 }
