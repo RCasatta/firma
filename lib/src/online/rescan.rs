4f90aa8824dc929fd87263b5c330eacfa0f11b2f
Riccardo Casatta <riccardo@casatta.it>
2021-02-12 14:01:18 +0100

    default to segwit activation block for rescan

--- lib/src/online/rescan.rs
+++ lib/src/online/rescan.rs
@@ -1,4 +1,5 @@
 use crate::OnlineContext;
+use bitcoin::Network;
 use bitcoincore_rpc::RpcApi;
 use serde_json::Value;
 use structopt::StructOpt;
@@ -10,15 +11,26 @@ pub struct RescanOptions {
     pub wallet_name: String,
 
     /// Specify the block height from which doing a blockchain rescan (use 0 to start from the beginning)
+    /// if missing will default to network segwit activation block
     #[structopt(long)]
-    pub start_from: usize,
+    pub start_from: Option<usize>,
 }
 
 impl OnlineContext {
     pub fn rescan(&self, opt: &RescanOptions) -> crate::Result<Value> {
+        let start_from = opt.start_from.unwrap_or_else(|| {
+            // segwith activation block
+            match self.network {
+                Network::Bitcoin => 481824,
+                Network::Testnet => 834624,
+                Network::Regtest => 0,
+                Network::Signet => 0,
+            }
+        });
+
         let (_a, b) = self
             .make_client(&opt.wallet_name)?
-            .rescan_blockchain(Some(opt.start_from), None)?;
+            .rescan_blockchain(Some(start_from), None)?;
         Ok(b.ok_or(crate::Error::MissingRescanUpTo)?.into())
     }
 }
